e6c01d4a3a0f7a63eb4360a6e7a04a702cd913d7
Johann <johannpino@gmail.com>
2018-04-16 00:58:23 -0300

    Terminamos estilos principales en sass, agregamos nosotro y contacto

--- webpack.config.js
+++ webpack.config.js
@@ -24,10 +24,7 @@ module.exports = {
                 test:/\.scss$/,
                 use:['style-loader','css-loader','sass-loader']
             },
-            {
-                test:/\.jpg$/,
-                loader:'url-loader'
-            }
+            { test: /\.jpg$/, loader: 'url-loader' }
         ]
     }
 }
